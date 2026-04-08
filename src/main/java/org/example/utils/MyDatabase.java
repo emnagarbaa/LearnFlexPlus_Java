3f0f2ee048161ab19f5dc8c380ae2a89b6d710af
Emna Garbaa <163394474+emna147852369oeirjfemrzo@users.noreply.github.com>
2026-04-08 18:32:50 +0100

    Premier commit

---
+++ src/main/java/org/example/utils/MyDatabase.java
@@ -0,0 +1,34 @@
+package org.example.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDatabase {
+    private final String URL = "jdbc:mysql://localhost:3306/learnflexplus";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "";
+
+    private Connection connection;
+    private static MyDatabase instance;
+
+    public MyDatabase() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connected to database successfully");
+        } catch (SQLException e) {
+            System.out.println("Error: failed to connect to database" + e.getMessage());
+        }
+    }
+
+    public static MyDatabase getInstance() {
+        if (instance == null) {
+            instance = new MyDatabase();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
